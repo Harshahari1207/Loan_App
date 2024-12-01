62308acdb71dec353c11e2f49e2cb342121aec4e
Harshahari1207 <harsha733084@gmail.com>
2024-12-01 09:12:54 +0530

    Start's on port 8082.

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
-const express = require('express');
 
-const app = express();
-
-app.use(express.json());
-
+const port = 8082;
+const app = require('./app');
 
 mongoose.connect('mongodb+srv://harsha733084:4AIcDBPqoyMdmb0h@loanapp.i8dcg.mongodb.net/?retryWrites=true&w=majority&appName=loanapp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB Connected...'))
+.then(() => {
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+    console.log('MongoDB Connected...')})
 .catch(err => console.log(err));
