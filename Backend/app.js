7be1c86bec30dca7d619a2b4fa0337863f70cb93
Harshahari1207 <harsha733084@gmail.com>
2024-12-01 17:11:37 +0530

    register and login routes setup.

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
+const routes = require("./route/index");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", require("./routes"));
+app.use("/api", routes);
 module.exports = app;
