aa8a0b5fcd15bb11b2c07cc1b1e41544b9f9a3e9
Harshahari1207 <harsha733084@gmail.com>
2025-01-23 12:24:40 +0530

    update loan by admin on approval and cancel loan.

--- Backend/route/adminRoute.js
+++ Backend/route/adminRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require("../controller/authController");
 const AdminController = require("../controller/adminController");
+const loanController = require("../controller/loanController");
 const { auth } = require("../middleware/auth");
 
 router.post("/register", AuthController.adminRegister);
 router.post("/login", AuthController.adminLogin);
 router.get("/allLoans", auth, AdminController.getAllLoans);
-
+router.put("/updateLoan/:id", auth, loanController.updateLoan);
 module.exports = router;
