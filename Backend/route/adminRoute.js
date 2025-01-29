220683feaaaa382d85f0cdbe314b47905fa93101
Harshahari1207 <harsha733084@gmail.com>
2025-01-29 12:07:24 +0530

    getall users api endpoint for admin.

--- Backend/route/adminRoute.js
+++ Backend/route/adminRoute.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const AuthController = require("../controller/authController");
 const AdminController = require("../controller/adminController");
 const loanController = require("../controller/loanController");
+const UserController = require("../controller/userController");
 const { auth } = require("../middleware/auth");
 
 router.post("/register", AuthController.adminRegister);
 router.post("/login", AuthController.adminLogin);
 router.get("/allLoans", auth, AdminController.getAllLoans);
+router.get("/users", auth, UserController.getAllUsers);
 router.put("/updateLoan/:id", auth, loanController.updateLoan);
 module.exports = router;
