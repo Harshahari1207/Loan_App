ba3b0ef5d3416d1f0852a19a67bf848032301f90
Harshahari1207 <harsha733084@gmail.com>
2025-02-09 10:37:19 +0530

    GetLoans by customer Id.

--- Backend/route/adminRoute.js
+++ Backend/route/adminRoute.js
@@ -10,5 +10,6 @@ router.post("/register", AuthController.adminRegister);
 router.post("/login", AuthController.adminLogin);
 router.get("/allLoans", auth, AdminController.getAllLoans);
 router.get("/users", auth, UserController.getAllUsers);
+router.get("/loans/:id", auth, loanController.getLoansByCustomerId);
 router.put("/updateLoan/:id", auth, loanController.updateLoan);
 module.exports = router;
